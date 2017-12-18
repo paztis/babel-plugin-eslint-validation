3ac4031e8cdf6a305430228a30d72daa4ee19cff
Jérôme HENAFF <jerome.henaff@amdocs.com>
2017-12-18 15:32:21 +0100

    first version

--- src/index.js
+++ src/index.js
@@ -1,36 +1,105 @@
-const CLIEngine = require("eslint").CLIEngine;
+const CACHE = {};
 
+/**
+ * Class representing an ESLintError.
+ * @extends Error
+ */
+class ESLintError extends Error {}
+
+
+const initOptions = (opts) => {
+    const {
+        fix = false,
+        formatter = 'stylish',
+        eslintPath = 'eslint',
+        failOnError = true,
+        failOnWarning = false
+    } = opts;
+
+    return {
+        fix,
+        formatter,
+        eslintPath,
+        failOnError,
+        failOnWarning
+    };
+};
+
+const getESLint = (options) => {
+    return require(options.eslintPath);
+};
+
+const initCli = (options) => {
+    const eslint = getESLint(options);
+    return new eslint.CLIEngine({
+        fix: options.fix
+    });
+};
+
+const execLint = (cli, input, resourcePath) => {
+    return cli.executeOnText(input, resourcePath, true);
+};
+
+const fixLint = (report, options) => {
+    if (options.fix && report.results && report.results[0].output) {
+        const eslint = getESLint(options);
+        eslint.CLIEngine.outputFixes(report);
+    }
+};
+
+const getFirstFailingMessage = (options, results, onlyErrors) => {
+    if (onlyErrors) {
+        const eslint = getESLint(options);
+        results = eslint.CLIEngine.getErrorResults(results);
+    }
+
+    const [errorItem = {}] = results;
+    const [message] = (errorItem.messages || []);
+
+    return message;
+};
 
 const visitor = () => {
-     return {
-         visitor: {
-             Program(path, state) {
-                 const fix = state.opts.fix;
-                 if (state.filename) {
-                     // Create ESLint instance
-                     const cli = new CLIEngine({
-                         fix: !!fix
-                     });
-                     const report = cli.executeOnFiles([state.filename]);
+    return {
+        visitor: {
+            Program(path, state) {
+                // ------------- Init cache ----------
+                if (!CACHE.options) {
+                    CACHE.options = initOptions(state.opts);
+                }
+                if (!CACHE.cli) {
+                    CACHE.cli = initCli(CACHE.options);
+                }
 
-                     // auto-fix the code
-                     if (fix && report.results && report.results[0].output) {
-                         CLIEngine.outputFixes(report);
-                     }
+                const {file} = state;
+                const {opts, code} = file;
+                const {cli, options} = CACHE;
 
-                     if (report.errorCount > 0) {
-                         // hide result filepath to avoid pollute the error display
-                         report.results.forEach(result => {
-                             result.filePath = '';
-                         });
+                const report = execLint(cli, code, opts.filename);
+                const isFailingError = (options.failOnError && report.errorCount > 0);
+                const isFailingWarning = (options.failOnWarning && report.warningCount > 0);
 
-                         const formatter = cli.getFormatter();
-                         throw path.buildCodeFrameError(formatter(report.results));
-                     }
-                 }
-             }
-         }
-     };
+                // auto-fix the code
+                fixLint(report, options);
+
+                if (isFailingError || isFailingWarning) {
+                    const formatter = cli.getFormatter(options.formatter);
+                    const formattedMessage = formatter(report.results);
+
+                    const failingMessage = getFirstFailingMessage(options, report.results, !isFailingWarning);
+
+                    const error = path.buildCodeFrameError(formattedMessage, ESLintError);
+                    if (failingMessage && error.loc && error.loc.line) {
+                        // Update error location
+                        error.loc.line = failingMessage.line;
+                        error.loc.column = failingMessage.column;
+                    }
+
+                    throw error;
+                }
+            }
+        }
+    };
 };
 
 
